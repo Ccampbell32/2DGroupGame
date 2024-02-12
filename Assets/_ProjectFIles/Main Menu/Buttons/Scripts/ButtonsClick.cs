77395f084fd1c4e75e74de4f92df9d9891e51397
T-465 <Dunne-T2@ulster.ac.uk>
2024-02-12 21:43:39 +0000

    Added basic main menu and script
    Barebones main menu added with stock graphic and working Play and Quit button

---
+++ Assets/_ProjectFIles/Main Menu/Buttons/Scripts/ButtonsClick.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsClick : MonoBehaviour
+{
+    public int sceneBuildIndex;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+
+
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+
+
+    
+
+       
+       
+            
+
+
+        
+    
+}
