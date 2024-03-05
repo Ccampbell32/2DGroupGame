fec2ee77a82c35ddc364770cf3d5a004100da207
Oisinf0805 <Ferry-o2@ulster.ac.uk>
2024-03-05 10:52:11 +0000

    Yes

--- Assets/_ProjectFIles/Scripts/Game Manager/GameManager.cs
+++ Assets/_ProjectFIles/Scripts/Game Manager/GameManager.cs
@@ -2,10 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+public enum GameState
+{
+    Battling,
+    MainMenu,
+    Options,
+    Paused,
+    Running,
+    GameOver
+}
 public class GameManager : MonoBehaviour
 {
+    public GameState CurrentGameState; // current game state
+    public bool OverworldRunning = false;
+    public float PlayerMaxHealth = 10;
     public static GameManager manager;
-
+    public PlayerMovement PlayerMovement;
     void Awake()
     {
         if(manager == null)
@@ -18,11 +32,51 @@ public class GameManager : MonoBehaviour
         }
     }
 
-      
+    private void Gameplay()
+    {
+        OverworldRunning = true;
+        Debug.Log("In gameplay"); // delete this line after testing
+    }
+    private void GameOver()
+    {
+        OverworldRunning = false;
+        //throw new NotImplementedException();
+    }
+    private void Battling()
+    {
+        OverworldRunning = false;
+        //throw new NotImplementedException();
+    }
+    private void MainMenu()
+    {
+        OverworldRunning = false;
+        PlayerMovement.enabled = false;
+        
+        
+    }
+    private void Options()
+    {
+        OverworldRunning = false;
+        //throw new NotImplementedException();
+    }
+    private void Paused()
+    {
+        OverworldRunning = false;
+        //throw new NotImplementedException();
+    }
+
+    // Method to change the game state
+    public void ChangeGameState(GameState newGameState)
+    {
+        CurrentGameState = newGameState;
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
+        CurrentGameState = GameState.MainMenu;
+        MainMenu();
         
     }
 
