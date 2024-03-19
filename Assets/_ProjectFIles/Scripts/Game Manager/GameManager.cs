37c4380ef12b3fadaa4a4a2ac4b6f08735b5d2e4
T-465 <Dunne-T2@ulster.ac.uk>
2024-03-19 11:11:02 +0000

    touchy touchy

--- Assets/_ProjectFIles/Scripts/Game Manager/GameManager.cs
+++ Assets/_ProjectFIles/Scripts/Game Manager/GameManager.cs
@@ -35,7 +35,13 @@ public class GameManager : MonoBehaviour
     public string unitname;
     public int unitLevel;
 
+    // different damage 
     public int damage;
+    public int damage2 = 3;
+    public int damage3 = 4;
+    public int damage4 = 5;
+
+    //player health
     public int maxHP;
     public int currentHP;
     public int CurrentXP;
@@ -207,12 +213,7 @@ public class GameManager : MonoBehaviour
         }
     }
     #endregion
-    public void PlayerStats()
-    {
 
-
-
-    }
     #region Take Damage 
     public bool TakeDamage(int dmg)
     {
@@ -228,6 +229,55 @@ public class GameManager : MonoBehaviour
             return false;
         }
     }
+    public bool TakeDamage2(int dmg2)
+    {
+        currentHP -= dmg2;
+        if (currentHP <= 0)
+        {
+            return true;
+
+        }
+        else
+        {
+
+            return false;
+        }
+
+    }
+    public bool TakeDamage3(int dmg3)
+    {
+        currentHP -= dmg3;
+        if (currentHP <= 0)
+        {
+            return true;
+
+        }
+        else
+        {
+
+            return false;
+        }
+
+    }
+    public bool TakeDamage4(int dmg4)
+    {
+        currentHP -= dmg4;
+        if (currentHP <= 0)
+        {
+            return true;
+
+        }
+        else
+        {
+
+            return false;
+        }
+
+    }
+
+
+
+
     #endregion
 
     #region freeze enemies
@@ -258,6 +308,12 @@ public class GameManager : MonoBehaviour
         }
     }
     #endregion
+
+
+
+
+
+
 }
 
 
