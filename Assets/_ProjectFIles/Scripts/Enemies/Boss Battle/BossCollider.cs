1519b3cd60d06869da5f6dc35e7c3077127c62c2
T-465 <Dunne-T2@ulster.ac.uk>
2024-04-01 14:34:22 +0100

    Boss Battle should work, rooms other than 3 gamemanager cant find battlesys when in battle

--- Assets/_ProjectFIles/Scripts/Enemies/Boss Battle/BossCollider.cs
+++ Assets/_ProjectFIles/Scripts/Enemies/Boss Battle/BossCollider.cs
@@ -6,13 +6,16 @@ public class BossCollider : MonoBehaviour
 {
     public GameObject player;
     public GameManager gameManager;
+    public BattleSystem battleSystem;
 
     private void Awake()
     {
-        gameManager = GetComponent<GameManager>();
+        
+        battleSystem = GameObject.FindWithTag("BattleSystem").GetComponent<BattleSystem>();
     }
     void Start()
     {
+        gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
         if (GameManager.manager != null)
         {
             gameManager = GameManager.manager;
@@ -22,12 +25,40 @@ public class BossCollider : MonoBehaviour
         {
             Debug.Log("GameManager not found! - Please add a GameManager to the scene!");
         }
+        if (battleSystem != null)
+        {
+            battleSystem = GameObject.FindWithTag("BattleSystem").GetComponent<BattleSystem>(); 
+
+        }
+        else
+        {
+            Debug.Log("BattleSystem not found! - Please add to the scene!");
+        }
+
+
         if (gameManager.BossBeaten == true)
         {
             gameObject.SetActive(false);
 
         }
 
+
+    }
+    private void Update()
+    {
+        if (gameManager.BossBeaten == true)
+        {
+            gameObject.SetActive(false);
+
+        }
+        else
+        {
+            gameObject.SetActive(true);
+        }
+        if (battleSystem.state == BattleState.WON)
+        {
+            gameManager.BossBeaten = true;
+        }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
