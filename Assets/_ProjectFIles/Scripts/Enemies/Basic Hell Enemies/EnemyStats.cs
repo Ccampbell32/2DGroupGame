fb6bdf49d293c2158ff2a6a10b7f50a4d49dcd2b
T-465 <Dunne-T2@ulster.ac.uk>
2024-04-01 15:13:05 +0100

    Enemy has defeat animation but the multiple enemy spawn problem is now an actual problem

--- Assets/_ProjectFIles/Scripts/Enemies/Basic Hell Enemies/EnemyStats.cs
+++ Assets/_ProjectFIles/Scripts/Enemies/Basic Hell Enemies/EnemyStats.cs
@@ -16,10 +16,29 @@ public class EnemyStats : MonoBehaviour
 
     public int XPheld;
 
+    //animation
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
     private void Update()
     {
         int randomNumber = Random.Range(1, 4);
         damage = randomNumber;
+
+
+        if (currentHP == 0)
+        {
+            animator.SetBool("IsDefeated", true);
+
+        }
+        else
+        {
+            animator.SetBool("IsDefeated", false);
+        }
     }
 
     public bool TakeDamage(int dmg)
@@ -37,4 +56,6 @@ public class EnemyStats : MonoBehaviour
         }
 
     }
+
+    
 }
