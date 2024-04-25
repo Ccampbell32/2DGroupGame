a9495d9e32660e5e05537afba5c45f1f5584cb8c
Oisinf0805 <Ferry-o2@ulster.ac.uk>
2024-04-25 10:29:21 +0100

    G

--- Assets/_ProjectFIles/Scripts/Player/Collisions/LevelSwitch.cs
+++ Assets/_ProjectFIles/Scripts/Player/Collisions/LevelSwitch.cs
@@ -8,18 +8,54 @@ public class LevelSwitch : MonoBehaviour
     public int sceneBuildIndex;
     public Transform PlayerSpawnPoint;
     public Transform newSpawn;
+    private bool IfLevelComplete;
+    public PlayerMovement playerMove;
+    public GameObject LevelCompleteUI;
 
+    public void Start()
+    {
+        Debug.Log("Level Switch start");
+        if (GameObject.FindWithTag("LevelComplete") != null)
+        {
+            LevelCompleteUI = GameObject.FindWithTag("LevelComplete");
+            playerMove = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+            LevelCompleteUI.SetActive(false);
+            IfLevelComplete = true;
+        }
+        else
+        {
+            IfLevelComplete = false;
+        }
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Entered");
 
         if(collision.tag == "Player")
         {
-            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+            if (IfLevelComplete)
+            {
+                LevelComplete();
+            }
+            else
+            {
+                SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+            }
 
            // PlayerSpawnPoint.transform.position = newSpawn.transform.position;
 
         }
+    }
+
+    public IEnumerator LevelComplete()
+        {
+            playerMove.enabled = false;
+            LevelCompleteUI.SetActive(true);
+            yield return new WaitForSeconds(2);
+            playerMove.enabled = true;
+            LevelCompleteUI.SetActive(false);
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
     }
 
 }
